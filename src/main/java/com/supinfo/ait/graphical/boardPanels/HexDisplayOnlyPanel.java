2072d7b245fdb7ca07d21567bc3e6b0ebd166c08
arsene <mcarsene16@yahoo.fr>
2015-03-11 11:12:56 +0100

    arsene 11/03/2015  à 11H12

---
+++ src/main/java/com/supinfo/ait/graphical/boardPanels/HexDisplayOnlyPanel.java
@@ -0,0 +1,11 @@
+package com.supinfo.ait.graphical.boardPanels;
+
+import com.supinfo.ait.hexBoards.Board;
+
+@SuppressWarnings("serial")
+public class HexDisplayOnlyPanel extends HexPanel{
+  public HexDisplayOnlyPanel(Board board){
+    super(board);
+  }
+
+}
---
+++ src/main/java/com/supinfo/ait/players/AbstractPlayer.java
@@ -0,0 +1,28 @@
+package com.supinfo.ait.players;
+
+import com.supinfo.ait.gameMechanics.Runner;
+import com.supinfo.ait.hexBoards.Board;
+import java.util.ArrayList;
+
+public abstract class AbstractPlayer implements Player {
+
+  protected ArrayList<Board> auxBoards = new ArrayList<Board>();
+  protected Runner game;
+  protected int player;
+  protected int opponent;
+  protected int size;
+
+  public AbstractPlayer(Runner game, int colour, String[] args) {
+    this.game = game;
+    this.player = colour;
+    if (this.player == Board.RED)
+      this.opponent = Board.BLUE;
+    else
+      this.opponent = Board.RED;
+    this.size = game.getBoard().getSize();
+  }
+
+  public ArrayList<Board> getAuxBoards() {
+    return auxBoards;
+  }
+}
